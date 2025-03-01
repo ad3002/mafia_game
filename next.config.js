db29218c2f8b07b8ffe5e118863535502a2ace8e
Aleksey Komissarov <ad3002@gmail.com>
2025-03-01 10:27:20 +0200

    Refactor configuration files and update game specifications

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { webpack }) => {
+    config.ignoreWarnings = [
+      { module: /node_modules/, message: /\[DEP0040\]/ }
+    ];
+    return config;
+  },
 }
 
 module.exports = nextConfig
